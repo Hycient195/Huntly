09ceef2abae8f21ce3ca357f4098a58398b729d9
Hycient195 <onyeukwuhycient@gmail.com>
2023-12-05 18:26:43 +0100

    Made some fixes in overview page

--- src/app/(withoughtNavigation)/dashboard/overview/page.tsx
+++ src/app/(withoughtNavigation)/dashboard/overview/page.tsx
@@ -5,15 +5,15 @@ import { memo } from "react";
 const OverviewPage = memo(() => {
   return (
     <main className="p-3 md:p-4 xl:p-6  grid grid- md:grid-rows-[1fr_4fr_7fr] gap-6 md:gap-4 h-[calc(100vh-100px)] max-h-[calc(100vh-100px)] lg:gap-5">
-      <section className="flex flex-row items-start gap-2 justify-between">
+      <section className="flex flex-row items-start gap-4 justify-between">
         <div className="header">
           <h1 className="header-title text-2xl font-bold text-slate-700">Good morning, Jake</h1>
-          <p className="header-subtitle leading-5 text-slate-500">
+          <p className="header-subtitle leading-5 text-sm text-slate-500">
             Here is what&apos;s happening with your job search applications from July
             19 - July 25.
           </p>
         </div>
-        <button className="date-range border border-slate-400/70 flex flex-row min-w-max gap-1.5 px-2 py-1 rounded items-center text-slate-500">
+        <button className="date-range border flex flex-row min-w-max gap-1.5 px-2 py-1 rounded items-center text-slate-500">
           <span className="date-range-text">Jul 19 - Jul 25</span>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
